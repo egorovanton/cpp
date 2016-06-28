d9f0d5b7c29e757ecb97e00eecea81fd1c552557
egorovanton <yegorov.ant@gmail.com>
2016-06-28 15:02:31 +0400

    Make strings changeable

--- task6/LazyString.cpp
+++ task6/LazyString.cpp
@@ -1,4 +1,39 @@
+#include <iostream>
+#include <memory>
+#include <string>
 #include "LazyString.h"
+
+LazyString::CharPointer::CharPointer(LazyString* ls, size_t pos) {
+    this->ls = ls;
+    this->pos = pos;
+}
+
+LazyString::CharPointer& LazyString::CharPointer:: operator = (char c) {
+    if (this->pos >= ls->len)
+        throw std::out_of_range("LazyString: out of range");
+
+    if (ls->string.use_count() > 1) {
+        ls->string = std::make_shared<std::string>(ls->string->substr(ls->start, ls->len));
+        ls->start = 0;
+    }
+
+    (*ls->string)[ls->start + this->pos] = c;
+    return *this;
+}
+
+LazyString::CharPointer::operator char() const {
+    if (this->pos >= ls->len)
+        throw std::out_of_range("LazyString: out of range");
+
+    return (*ls->string)[ls->start + this->pos];
+}
+
+LazyString::LazyString(const std::shared_ptr<std::string> string, size_t start, size_t len) {
+    this->string = string;
+    this->start  = start;
+    this->len    = len;
+
+}
 
 LazyString::LazyString() {
     start = 0;
@@ -6,67 +41,63 @@ LazyString::LazyString() {
     string = std::make_shared<std::string>("");
 }
 
-LazyString::LazyString(const std::string& str) {
-    start = 0;
-    len = str.length();
-    string = std::make_shared<std::string>(str);
+
+LazyString::LazyString(const std::string& string) {
+    this->start  = 0;
+    this->len    = string.length();
+    this->string = std::make_shared<std::string>(string);
 }
 
-LazyString::LazyString(const LazyString& lazyStr) {
-    start = lazyStr.start;
-    len = lazyStr.length();
-    string = lazyStr.string;
+size_t LazyString::size() const {
+    return this->len;
 }
 
-LazyString::LazyString(const std::shared_ptr<std::string>& str, size_t pos, size_t len) {
-    string = str;
-    start = pos;
-    this->len = len;
+size_t LazyString::length() const {
+    return this->len;
 }
 
-LazyString::operator std::string() {
-    return std::string(string->substr(start, len));
+LazyString::CharPointer LazyString::at(size_t pos) {
+    return CharPointer(this, pos);
 }
 
-char& LazyString::operator [] (size_t pos) {
-    return string->at(pos);
+const char& LazyString::at(size_t pos) const {
+    if (pos >= this->len)
+        throw std::out_of_range("LazyString: out of range");
+
+    return (*this->string)[this->start + pos];
+}
+
+LazyString::CharPointer LazyString::operator [] (size_t pos) {
+    return at(pos);
 }
 
 const char& LazyString::operator [] (size_t pos) const {
     return at(pos);
 }
 
-const char& LazyString::at (size_t pos) const {
-    if (pos + start > len) {
-        throw std::out_of_range("LazyString::at()");
-    }
-    return string->at(pos + start);
-}
-
-size_t LazyString::length() const {
-    return len - start;
-}
-
-size_t LazyString::size() const {
-    return len - start;
-}
-
 LazyString LazyString::substr(size_t pos, size_t len) const {
-    return LazyString(string, pos, len);
+    if (start >= this->len)
+        throw std::out_of_range("LazyString: out of range");
+
+    return LazyString(this->string, this->start + pos, pos + len > this->len ? this->len - pos : len);
 }
 
-
-std::istream& operator >> (std::istream& input, LazyString& lazyString) {
-    input >> *(lazyString.string);
-    lazyString.start = 0;
-    lazyString.len = lazyString.string->length();
-    return input;
+LazyString::operator std::string() {
+    return std::string(this->string->substr(this->start, this->len));
 }
 
-std::ostream& operator << (std::ostream& output, LazyString& lazyString) {
+std::ostream& operator << (std::ostream &output, LazyString &lazyString) {
     output << lazyString.string->substr(lazyString.start, lazyString.len);
     return output;
 }
 
+std::istream& operator >> (std::istream &input, LazyString &lazyString) {
+    auto string = std::make_shared<std::string>();
 
+    input >> *string;
+    lazyString.string = string;
+    lazyString.start = 0;
+    lazyString.len = string->length();
 
+    return input;
+}
--- task6/LazyString.h
+++ task6/LazyString.h
@@ -1,34 +1,52 @@
 #ifndef LAZYSTRING_H
 #define LAZYSTRING_H
 
+#include <iostream>
 #include <memory>
 #include <string>
-#include <stdexcept>
 
-class LazyString
+class LazyString 
 {
-    public:
-        LazyString();
-        LazyString(const std::string& str);
-        LazyString(const LazyString& lazyStr);
-
-        operator std::string();
-
-        char& operator [] (size_t pos);
-        const char& operator[] (size_t pos) const;
-        const char& at (size_t pos) const;
-
-        size_t length() const;
-        size_t size() const;
-
-        LazyString substr(size_t pos, size_t len) const;
-
-        friend std::istream& operator >> (std::istream& input, LazyString& lazyString);
-        friend std::ostream& operator << (std::ostream& output, LazyString& lazyString);
+private:
+    struct CharPointer
+    {
+        friend class LazyString;
     private:
-        LazyString (const std::shared_ptr<std::string>& str, size_t pos, size_t len);
-        std::shared_ptr<std::string> string;
-        size_t start, len;
+        size_t pos;
+        LazyString* ls;
+        
+        CharPointer(LazyString* ls, size_t pos);
+    public:
+        operator char() const;
+
+        CharPointer& operator = (char c);
+    };
+    
+    std::shared_ptr<std::string> string;
+    size_t start;
+    size_t len;
+
+    LazyString(const std::shared_ptr<std::string> string, size_t start, size_t len);
+public:
+    LazyString();
+
+    LazyString(const std::string& string);
+
+    size_t size() const;
+    size_t length() const;
+
+    CharPointer at(size_t pos);
+    const char& at(size_t pos) const;
+
+    CharPointer operator[](size_t pos);
+    const char& operator[](size_t pos) const;
+    
+    LazyString substr(size_t pos = 0, size_t len = std::string::npos) const;
+    
+    operator std::string();
+
+    friend std::istream& operator >> (std::istream& is, LazyString& ls);
+    friend std::ostream& operator << (std::ostream& os, LazyString& ls);
 };
 
 #endif // LAZYSTRING_H
