055d1e502e24a1c7177622e275fef95778c7dc8c
egorovanton <yegorov.ant@gmail.com>
2016-06-28 17:29:03 +0400

    Add files via upload

--- task7/LazyString.cpp
+++ task7/LazyString.cpp
@@ -1,169 +1,172 @@
-#include <atomic>
-#include <iostream>
 #include <memory>
-#include <string>
+#include <iostream>
+#include <atomic>
 #include <thread>
+#include <string>
 #include "LazyString.h"
 
-LazyLock::LazyLock() {
+StringLock::StringLock() {
     readers = 0;
 }
 
-LazyLock::LazyLock(const LazyLock& lock) {
+StringLock::StringLock(const StringLock& lock) {
     readers = 0;
-}                                                       
+}                                                      
 
-void LazyLock::readLock() {
+void StringLock::readLock() {
     this->mutex.lock();
     this->readers++;
     this->mutex.unlock();
 }
 
-void LazyLock::readUnlock() {
-    this->readers--;
-}
-
-void LazyLock::writeLock() {
+void StringLock::writeLock() {
     this->mutex.lock();
-    while (this->readers.load() > 0) {
+    while (this->readers.load() > 0)
+    {
         std::this_thread::yield();
     }
 }
 
-void LazyLock::writeUnlock() {
+void StringLock::readUnlock() {
+    this->readers--;
+}
+
+void StringLock::writeUnlock() {
     this->mutex.unlock();
 }
 
-LazyString::LazyChar::LazyChar(LazyString* ls, size_t pos) {
-    this->ls  = ls;
+LazyString::CharPointer::CharPointer(LazyString* str, size_t pos) {
+    this->str = str;
     this->pos = pos;
 }
 
-LazyString::LazyChar::operator char() const {
-    ls->lock->readLock();
-    if (this->pos >= ls->len)
+LazyString::CharPointer::operator char() const {
+    str->lock->readLock();
+    if (this->pos >= str->len) {
         throw std::out_of_range("LazyString: out of range");
-
-    char l_c = (*ls->string)[ls->start + this->pos];
-    ls->lock->readUnlock();
-    return l_c;
-}
-
-LazyString::LazyChar& LazyString::LazyChar::operator=(char c) {
-    ls->lock->writeLock();
-    if (this->pos >= ls->len)
-        throw std::out_of_range("LazyString: out of range");
-    std::shared_ptr<LazyLock> l_lock = ls->lock;
-
-    if (ls->string.useCount() > 1)
-    {
-        ls->string = std::make_shared<std::string>(ls->string->substr(
-            ls->start, ls->len));
-        ls->lock = std::make_shared<LazyLock>();
-        ls->start = 0;
     }
 
-    (*ls->string)[ls->start + this->pos] = c;
-    l_lock->writeUnlock();
+    char lchar = (*str->string)[str->start + this->pos];
+    str->lock->readUnlock();
+    return lchar;
+}
+
+LazyString::CharPointer& LazyString::CharPointer::operator = (char c) {
+    str->lock->writeLock();
+    if (this->pos >= str->len) {
+        throw std::out_of_range("LazyString: out of range");
+    }
+
+    auto newLock = str->lock;
+    if (str->string.use_count() > 1) {
+        str->string = std::make_shared<std::string>(str->string->substr(str->start, str->len));
+        str->lock = std::make_shared<StringLock>();
+        str->start = 0;
+    }
+
+    (*str->string)[str->start + this->pos] = c;
+    newLock->writeUnlock();
     return *this;
 }
 
-LazyString::LazyString( const std::shared_ptr<std::string> string, 
-                        const std::shared_ptr<LazyLock> lock,
-                        size_t start, size_t len )
-{
-    this->string = string,
+LazyString::LazyString(const std::shared_ptr<std::string> string, const std::shared_ptr<StringLock> lock, size_t start, size_t len) {
+    this->string = string;
     this->lock   = lock;
-    this->start  = start;
+    this->start  = 0;
     this->len    = len;
 }
 
-LazyString::LazyString() {
-    LazyString("");
+LazyString::LazyString() : LazyString(std::string()) {
 }
 
 LazyString::LazyString(const std::string& string) {
     this->string = std::make_shared<std::string>(string);
-    this->lock = std::make_shared<LazyLock>();
-    start = 0;
-    this->len = string.length();
+    this->lock   = std::make_shared<StringLock>();
+    this->start  = 0;
+    this->len    = string.length();
 }
 
 size_t LazyString::length() const {
-    this->lock->readLock();
-    size_t llen = this->len;
-    this->lock->readUnlock();
-    return llen;
+    return size();
 }
 
 size_t LazyString::size() const {
-    return length();
+    this->lock->readLock();
+    size_t len = this->len;
+    this->lock->readUnlock();
+    return len;
 }
 
-LazyString::LazyChar LazyString::at(size_t pos) {
-    return LazyChar(this, pos);
+LazyString::CharPointer LazyString::operator [] (size_t pos) {
+    return at(pos);
+}
+
+const char& LazyString::operator [] (size_t pos) const {
+    return at(pos);
+}
+
+LazyString::CharPointer LazyString::at(size_t pos) {
+    return CharPointer(this, pos);
 }
 
 const char& LazyString::at(size_t pos) const {
     this->lock->readLock();
-    if (pos >= this->len)
+    if (pos >= this->len) {
         throw std::out_of_range("LazyString: out of range");
+    }
 
     char& lchar = (*this->string)[this->start + pos];
     this->lock->readUnlock();
     return lchar;
 }
 
-LazyString::LazyChar LazyString::operator[](size_t pos) {
-    return at(pos);
-}
 
-const char& LazyString::operator[](size_t pos) const {
-    return at(pos);
+
+LazyString::operator std::string() {
+    this->lock->readLock();
+    std::string str = std::string(this->string->substr(this->start, this->len));
+    this->lock->readUnlock();
+    return str;
 }
 
 LazyString LazyString::substr(size_t pos, size_t len) const {
     this->lock->readLock();
-    if (start >= this->len)
+    if (start >= this->len) {
         throw std::out_of_range("LazyString: out of range");
+    }
 
-    std::shared_ptr<std::string> lazyString = this->string;
-    std::shared_ptr<LazyLock> l_lock = this->lock;
-    size_t l_start = this->start;
-    size_t l_len = this->len;
+    size_t lockedStart = this->start;
+    size_t lockedLen   = this->len;
+
+    auto newLock      = this->lock;
+    auto lockedString = this->string;
 
     this->lock->readUnlock();
-    return LazyString(lazyString, l_lock, l_start + pos, pos + len > l_len ? l_len - pos : len);
+    return LazyString(lockedString, newLock, lockedStart + pos, pos + len > lockedLen ? lockedLen - pos : len);
 }
 
-LazyString::operator std::string() {
-    this->lock->readLock();
-    std::string lazyString = std::string(this->string->substr(this->start,
-                                                            this->len));
-    this->lock->readUnlock();
-    return lazyString;
-}
-
-std::istream& operator>>(std::istream &input, LazyString &lazyString) {
+std::istream& operator >> (std::istream &input, LazyString &str) {
     auto string = std::make_shared<std::string>();
 
     input >> *string;
-    lazyString.lock->writeLock();
-    std::shared_ptr<LazyLock> l_lock = lazyString.lock;
+    str.lock->writeLock();
+    std::shared_ptr<StringLock> newLock = str.lock;
 
-    lazyString.string = string;
-    lazyString.lock = std::make_shared<LazyLock>();
-    lazyString.start = 0;
-    lazyString.len = string->length();
+    str.string = string;
+    str.lock = std::make_shared<StringLock>();
+    str.start = 0;
+    str.len = string->length();
 
-    l_lock->write_unlock();
+    newLock->writeUnlock();
     return input;
 }
 
-std::ostream& operator<<(std::ostream &output, LazyString &lazyString) {
-    lazyString.lock->readLock();
-    output << lazyString.string->substr(lazyString.start, lazyString.len);
-    lazyString.lock->readUnlock();
+std::ostream& operator << (std::ostream &output, LazyString &str) {
+    str.lock->readLock();
+    for (size_t i = 0; i < str.len; ++i) {
+        output << (*str.string)[str.start + i];
+    }
+    str.lock->readUnlock();
 
     return output;
 }
--- task7/LazyString.h
+++ task7/LazyString.h
@@ -1,70 +1,65 @@
 #ifndef LAZYSTRING_H
 #define LAZYSTRING_H
 
+#include <memory>
+#include <iostream>
+#include <mutex>
 #include <atomic>
 #include <string>
-#include <iostream>
-#include <memory>
-#include <mutex>
 
-class LazyLock
-{
+class StringLock {
     private:
         std::atomic<int> readers;
         std::mutex mutex;
     public:
-        LazyLock();
-        LazyLock(const LazyLock& lock);
+        StringLock();
+        StringLock(const StringLock& lock);
+        
         void readLock();
-        void readUnlock();
         void writeLock();
+        
+        void readUnlock();
         void writeUnlock();
 };
 
-class LazyString
-{
+class LazyString {
     private:
-        struct LazyChar
-        {
-            friend class LazyString;
-        private:
-            const size_t pos;
-            LazyString* ls;
-            
-            LazyChar(LazyString* ls, size_t pos);
-        public:
-            
-            operator char() const;
-            LazyChar& operator=(char c);
-        };
-        
-        size_t len;
         size_t start;
-
-        std::shared_ptr<std::string> string;
-        std::shared_ptr<LazyLock> lock;
+        size_t len;
         
-        LazyString( const std::shared_ptr<std::string> string,
-                    const std::shared_ptr<LazyLock> lock, size_t start,
-                    size_t len);
+        std::shared_ptr<std::string> string;
+        std::shared_ptr<StringLock> lock;
+
+        LazyString(const std::shared_ptr<std::string> string, const std::shared_ptr<StringLock> lock, size_t start, size_t len);
+        
+        struct CharPointer {
+            friend class LazyString;
+            private:
+                size_t pos;
+                LazyString* str;
+                CharPointer(LazyString* str, size_t pos);
+            public:
+                operator char() const;
+                CharPointer& operator = (char c);
+        };
+
     public:
-        LazyString();
-        LazyString(const std::string& string);
         size_t length() const;
         size_t size() const;
 
-        LazyChar at(size_t pos);
-        const char& at(size_t pos) const;
+        LazyString();
+        LazyString(const std::string& string);
 
         operator std::string();
-
-        LazyChar operator [] (size_t pos);
-        const char& operator [] (size_t pos) const;
-        
         LazyString substr(size_t pos = 0, size_t len = std::string::npos) const;
 
-        friend std::istream& operator >> (std::istream& is, LazyString& ls);        
-        friend std::ostream& operator << (std::ostream& os, LazyString& ls);
+        CharPointer operator [] (size_t pos);
+        const char& operator [] (size_t pos) const;        
+        CharPointer at(size_t pos);
+        const char& at(size_t pos) const;        
+
+        friend std::istream& operator >> (std::istream& input, LazyString& str);
+        friend std::ostream& operator << (std::ostream& output, LazyString& str);
 };
 
-#endif // LAZYSTRING_H
+#endif
